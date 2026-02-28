62d30ddb720ae79aac1919af6ab2f98d86859715
iristhepianist <gaurvi.sharma@icloud.com>
2026-03-01 02:05:01 +0530

    idk

--- 1122-Cleanroom/src/main/java/fulcrum/cleanroom1122/gen/Generator.java
+++ 1122-Cleanroom/src/main/java/fulcrum/cleanroom1122/gen/Generator.java
@@ -1,12 +1,13 @@
 package fulcrum.cleanroom1122.gen;
 
 import fulcrum.cleanroom1122.CleanroomModEntry;
+import fulcrum.cleanroom1122.gen.RecipeRegistrar;
 
 public class Generator {
 	public static void main(String[] args) {
-		//Create Instance
 		CleanroomModEntry cleanroomModEntry = new CleanroomModEntry();
 
 		ModelRegistrar.registerModels();
+		RecipeRegistrar.generateJsons();
 	}
 }
