5019221b74c9a6657c372390c7164f770569e518
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2026-03-04 21:30:28 -0500

    Update MixinWorld.java

--- 1710-Forge/src/main/java/fulcrum/forge1710/mixins/MixinWorld.java
+++ 1710-Forge/src/main/java/fulcrum/forge1710/mixins/MixinWorld.java
@@ -13,18 +13,18 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(World.class)
 public abstract class MixinWorld implements IMCWorld {
 	@Shadow
-	public abstract boolean setBlock$(int x, int y, int z, Block blockIn, int metadataIn, int flags);
+	public abstract boolean setBlock(int x, int y, int z, Block blockIn, int metadataIn, int flags);
 	@Shadow
 	public abstract TileEntity getTileEntity(int x, int y, int z);
 
 	@Override
 	public boolean setBlock$(IMCPos pos, IMCBlock block) {
-		return setBlock$(pos.getX$(), pos.getY$(), pos.getZ$(), (Block) block, 0, 2);
+		return setBlock(pos.getX$(), pos.getY$(), pos.getZ$(), (Block) block, 0, 2);
 	}
 
 	@Override
 	public boolean setBlock$(int x, int y, int z, IMCBlock block) {
-		return setBlock$(x, y, z, (Block) block, 0, 2);
+		return setBlock(x, y, z, (Block) block, 0, 2);
 	}
 
 	@Override
