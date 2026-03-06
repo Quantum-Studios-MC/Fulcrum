fff462ec8615997d895830dbec2370e9cdbe4888
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2026-03-05 20:46:26 -0500

    wrong ITickable

--- 1122-Cleanroom/src/main/java/fulcrum/cleanroom1122/game/tileentities/TileEntityTickableBase.java
+++ 1122-Cleanroom/src/main/java/fulcrum/cleanroom1122/game/tileentities/TileEntityTickableBase.java
@@ -1,15 +1,14 @@
 package fulcrum.cleanroom1122.game.tileentities;
 
 import fulcrum.api.tileentities.ITile;
-import net.minecraft.client.renderer.texture.ITickable;
 
-public class TileEntityTickableBase extends TileEntityBase implements ITickable {
+public class TileEntityTickableBase extends TileEntityBase implements net.minecraft.util.ITickable {
 	public TileEntityTickableBase(ITile tile) {
 		super(tile);
 	}
 
 	@Override
-	public void tick() {
+	public void update() {
 		this.parentTile.tick();
 	}
 }
