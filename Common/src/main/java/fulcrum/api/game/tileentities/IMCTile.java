b93a0d55f90dc312bc3e22ffb98c68300e2c5dd6
JohnButlerThird <112212674+JohnButlerThird@users.noreply.github.com>
2026-03-06 19:40:58 -0500

    fix

--- Common/src/main/java/fulcrum/api/game/tileentities/IMCTile.java
+++ Common/src/main/java/fulcrum/api/game/tileentities/IMCTile.java
@@ -3,4 +3,6 @@ package fulcrum.api.game.tileentities;
 import fulcrum.api.game.nbt.IMCTagCompound;
 
 public interface IMCTile {
+	default void read$(IMCTagCompound tag) {};
+	default void write$(IMCTagCompound tag) {};
 }
